fea4aba7afd37f74ccce9b17e621fbcb86ee8de5
VigneshDhandapani <VigneshDhandapani@users.noreply.github.com>
2014-10-01 07:07:46 +0530

    TUI
    TUI

---
+++ TUI.IDAL/IHomeDS.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TUI.DTO;
+using System.Collections.ObjectModel;
+
+namespace TUI.IDAL
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IHomeDS
+    {
+        /// <summary>
+        /// Plans the register.
+        /// </summary>
+        /// <param name="planning">The planning.</param>
+        void PlanRegister(Planning planning);
+        /// <summary>
+        /// Searches the planned.
+        /// </summary>
+        /// <param name="joinDO">The join do.</param>
+        /// <returns></returns>
+        Collection<Planning> SearchPlanned(JoinDO joinDO);
+        /// <summary>
+        /// Gets the state list.
+        /// </summary>
+        /// <returns></returns>
+        Collection<DataSource> GetStateList();
+        /// <summary>
+        /// Gets the city list.
+        /// </summary>
+        /// <param name="StateId">The state identifier.</param>
+        /// <returns></returns>
+        Collection<DataSource> GetCityList(int? StateId);
+        /// <summary>
+        /// Joins the tui.
+        /// </summary>
+        /// <param name="joinDO">The join do.</param>
+        void JoinTUI(JoinDO joinDO);
+    }
+}
